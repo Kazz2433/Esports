4a6906f58b5b3b9a00d90ef622c7941ff0143952
armit4ge <kelvinquida24@gmail.com>
2022-10-09 14:47:00 -0400

    configurando o back

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,20 @@
 import express from "express";
+import {PrismaClient} from '@prisma/client'
+
 
 const app = express()
+const prisma = new PrismaClient()
 
-app.get('/ads',(req,res) => {
+app.get('/games', (req,res) => {
+    const games = prisma.game.findMany()
+    return res.json([])
+})
+
+app.post('/ads', (req,res) => {
+    return res.status(201).json([])
+})
+
+app.get('/games/:id/ads',(req,res) => {
     return res.json([
         {id:1,name:"Anuncio 1"},
         {id:2,name:"Anuncio 2"},
@@ -10,6 +22,11 @@ app.get('/ads',(req,res) => {
         {id:4,name:"Anuncio 4"},
     ])
 })  
+ 
+app.get('/ads/:id/discord',(req,res) => {
+    return res.json([])
+})  
+
 
 
 app.listen(3333)
